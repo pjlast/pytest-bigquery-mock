c0a4a35fb08173e6e923159e016922c52404b068
Ed Morrell <morrell53@Eds-MacBook-Pro.local>
2023-02-04 13:01:50 +0000

    Added tests to implement the plugin

---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+from pytest_bigquery_mock.plugin import bq_client_mock
+
+plugins = ["pytest-bigquery-mock"]
---
+++ tests/test_plugin.py
@@ -0,0 +1,58 @@
+import pytest
+import pandas as pd
+
+
+def query_function(bq_client):
+    row_iter = bq_client.query("SELECT * FROM table").result()
+    return row_iter
+
+
+def load_table_function(bq_client, df):
+    load_job = bq_client.load_table_from_dataframe(df, "a_table")
+    print(load_job.state)
+    return load_job.done()
+
+
+@pytest.mark.bq_load_table_state(done_state=True, exist_state=True, running_state=False)
+def test_function_that_calls_bigquery_v2(bq_client_mock):
+
+    expected_row_dicts = [
+        {"id_row": 1, "name": "Alice"},
+        {"id_row": 2, "name": "Pete"},
+        {"id_row": 3, "name": "Steven"},
+    ]
+
+    df = pd.DataFrame(expected_row_dicts)
+
+    done = load_table_function(bq_client_mock, df)
+    assert done
+
+
+@pytest.mark.bq_query_return_data(
+    [
+        {
+            "query": "SELECT * FROM table",
+            "table": {
+                "columns": [
+                    "id_row",
+                    "name",
+                ],
+                "rows": [
+                    [1, "Alice"],
+                    [2, "Pete"],
+                    [3, "Steven"],
+                ],
+            },
+        },
+    ]
+)
+def test_function_that_calls_bigquery(bq_client_mock):
+    row_iter = query_function(bq_client_mock)
+
+    expected_row_dicts = [
+        {"id_row": 1, "name": "Alice"},
+        {"id_row": 2, "name": "Pete"},
+        {"id_row": 3, "name": "Steven"},
+    ]
+    for row, expected_row in zip(row_iter, expected_row_dicts):
+        assert dict(row) == expected_row
